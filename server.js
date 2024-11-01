00accf08080df547422e045c466f0e4c99921485
Sachinjha08 <139585222+Sachinjha08@users.noreply.github.com>
2024-11-02 00:59:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
 const connectDB = require("./config/connectDB");
-connectDB();
 const morgan = require("morgan");
 const cors = require("cors");
 const userRoute = require("./routes/userRoutes");
 const blogRoute = require("./routes/blogRoutes");
 const cookie = require("cookie-parser");
 const path = require("path");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -23,21 +24,16 @@ app.use(
 app.use(express.json());
 app.use(cookie());
 
-const PORT = process.env.PORT || 7000;
+// Serve static files
+app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
 
-// app.use("/", (req, res) => {
-//   res.status(201).send({
-//     message: `App is running on ${PORT}`,
-//   });
-// });
+// Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/blog", blogRoute);
 
 app.get("/", (req, res) => {
-  app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`);
-});
+// Export the app for Vercel
+module.exports = app;
