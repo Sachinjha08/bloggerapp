553bc6695645ecde65cfecc0ccbe9ab38146e3d6
Sachinjha08 <139585222+Sachinjha08@users.noreply.github.com>
2024-11-02 01:11:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
+const connectDB = require("./config/connectDB");
+connectDB();
+const morgan = require("morgan");
+const cors = require("cors");
+const userRoute = require("./routes/userRoutes");
+const blogRoute = require("./routes/blogRoutes");
+const cookie = require("cookie-parser");
 const path = require("path");
 
 const app = express();
 
+app.use(morgan("dev"));
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use(express.json());
+app.use(cookie());
+
 const PORT = process.env.PORT || 7000;
 
-// Serve static files from the 'frontend/dist' directory
+// Serve static files from the frontend's dist folder
 app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
 
-// Root route to serve index.html
-app.get("/", (req, res) => {
-  try {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  } catch (error) {
-    console.error("Error sending index.html:", error);
-    res.status(500).send("Server error occurred while loading the page.");
-  }
+// API routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/blog", blogRoute);
+
+// Serve the index.html file for the root route
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
